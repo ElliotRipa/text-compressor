e70dc9bdd4578a2984491bbed70b222dc16518e2
ElliotRipa <elliot.ripa@gmail.com>
2022-11-13 22:14:43 +0100

    Started implementing quicksort.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class Main {
     public static void main(String[] args) {
 
-         ArrayList<String> file = FileReader.getFile("C:\\Users\\ellio\\IdeaProjects\\text-compressor\\src\\filename.txt");
+        Hashtable<String, Integer> dict = new Hashtable<>();            //Creates a prevalence dictionary.
 
-         for(String word : file) {
-             System.out.println(word);
+         ArrayList<String> file = FileReader.getFile("C:\\Users\\ellio\\IdeaProjects\\text-compressor\\src\\filename.txt");     //Reads a given file.
+
+        assert file != null;
+
+        for(String word : file) {
+             dict.merge(word, 1, Integer::sum);     //If a word does not already exist, sets its value to 1, increments otherwise.
          }
 
+        Tupler tplr = new Tupler();
+        tplr.getSorted(dict);
+
+        System.out.println("Haiii omg haiiii :3 uwuuw");
+
     }
+
+    private void generatePreamble(String filepath, Hashtable<String, Integer> dict) {
+
+        ArrayList<String> file = FileReader.getFile(filepath);
+
+    }
+
+    private void sortDictionary(Hashtable<String, Integer> dict) {
+
+    }
+
 }
--- src/Tupler.java
+++ src/Tupler.java
@@ -1,51 +1,65 @@
 import javafx.util.Pair;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Hashtable;
 import java.util.Set;
 
-public class Toupler {
+public class Tupler {
 
-    public static void sort(Hashtable<String, Integer> dict) {
+    public void getSorted(Hashtable<String, Integer> dict) {
+        ArrayList<Pair<String, Integer>> array = makeDictTuple(dict);
+
+        quickSort(array, 0, array.size());
 
     }
 
-    private ArrayList<Pair<String, Integer>> tupler(Hashtable<String, Integer> dict) {
-        ArrayList<Pair<String, Integer>> result = new ArrayList<Pair<String, Integer>>();
+
+    Comparator<? super Integer> comparator;
+
+
+    private ArrayList<Pair<String, Integer>> makeDictTuple(Hashtable<String, Integer> dict) {
+        ArrayList<Pair<String, Integer>> result = new ArrayList<>();
         Set<String> setOfKeys = dict.keySet();
         for(String key : setOfKeys) {
-            Pair<String, Integer> word = [key, dict.get(key)];
+            Pair<String, Integer> word = new Pair<>(key, dict.get(key));
+            result.add(word);
         }
+        return result;
     }
 
-    private void quickSort(Hashtable<String, Integer> dict, int lo, int hi) {
+    private void quickSort(ArrayList<Pair<String, Integer>> array, int lo, int hi) {
+
+        Util.CountingComparator<Integer> cmp = new Util.CountingComparator<Integer>(Comparator.naturalOrder());
+        this.comparator = cmp;
+
         // Base case
         int size = hi - lo;
         if (size <= 1)
             return;
-        int partitionIndex = partition(dict, lo, hi);
-        sort(dict, lo, partitionIndex);
-        sort(dict, partitionIndex+1, hi);
+        int partitionIndex = partition(array, lo, hi);
+        quickSort(array, lo, partitionIndex);
+        quickSort(array, partitionIndex+1, hi);
         return;
     }
 
-    private int partition(Hashtable<String, Integer> dict, int lo, int hi) {
+    private int partition(ArrayList<Pair<String, Integer>> array, int lo, int hi) {
         // pivotIndex is the index of the element that should be used
         // as the pivot.
         int pivotIndex = lo;
         Util.swap(array, lo, pivotIndex);
         pivotIndex = lo;
-        E pivot = array.get(pivotIndex);
+        Pair<String, Integer> pivot = array.get(pivotIndex);
         lo++;
         hi--;
 
         try {
-            while (lo <= hi) {
+            while (lo <= hi) {      //TODO: Not get stuck in infinite loop.
 
-                if ((comparator.compare(array.get(lo), pivot)) == -1) {
+                if ((comparator.compare(array.get(lo).getValue(), pivot.getValue())) == -1) {
                     lo++;
 
-                } else if ((comparator.compare(array.get(hi), pivot)) == 1) {
+                } else if ((comparator.compare(array.get(hi).getValue(), pivot.getValue())) == 1) {
                     hi--;
 
                 } else {
