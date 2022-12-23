cb7c1f836009ed68d2308c4a9551b6ae2dfbefe8
ElliotRipa <elliot.ripa@gmail.com>
2022-12-23 10:40:17 +0100

    Added methods getCharList() and makePQ().

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,24 +9,20 @@ public class Main {
         System.out.println("What's the filepath? ");
         Scanner scanner = new Scanner(System.in);
         String filepath = scanner.nextLine();
-        ArrayList<String> file = FileReader.getFile(filepath);     //Reads a given file.
+        ArrayList<String> wordList = FileReader.getFile(filepath);      //Reads a given file.
 
-        assert file != null;
+        assert wordList != null;
 
-        for(String word : file) {
-             dict.merge(word, 1, Integer::sum);     //If a word does not already exist, sets its value to 1, increments otherwise.
+        for(String word : wordList) {
+             dict.merge(word, 1, Integer::sum);                         //If a word does not already exist, sets its value to 1, increments otherwise.
          }
+
+        CharList cl = getCharList(wordList);                            //Generates a CharList for the given wordList.
+
 
         Tupler tplr = new Tupler();
         ArrayList<Pair<String, Integer>> list = tplr.getSorted(dict);
 
-        CharList al = new CharList();
-
-        al.bitWiseOr((char) 2);
-
-        al.bitWiseOr('?');
-
-        System.out.println(al.size());
 
         System.out.println("Haiii omg haiiii :3 uwuuw");                //Just so that I can set a breakpoint. I'm sorry.
 
@@ -38,6 +33,37 @@ public class Main {
         ArrayList<String> file = FileReader.getFile(filepath);
 
     }
+
+
+    private static CharList getCharList(ArrayList<String> words) {
+
+        CharList output = new CharList();
+
+        for(String word : words) {
+            char[] chars = word.toCharArray();
+
+            for(char c : chars) {
+                output.bitWiseOr(c);
+            }
+
+        }
+    return output;
+    }
+
+
+    private static PriorityQueue<PrevalenceEntry> makePQ(Hashtable<String, Integer> prevMap) {
+
+        PriorityQueue<PrevalenceEntry> PQ = new PriorityQueue<>();
+
+        Set<String> keySet = prevMap.keySet();
+
+        for(String key : keySet) {
+            PrevalenceEntry PE = new PrevalenceEntry(key, prevMap.get(key));
+            PQ.add(PE);
+        }
+        return PQ;
+    }
+
 
     private void sortDictionary(Hashtable<String, Integer> dict) {
 
